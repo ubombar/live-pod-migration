af002b3d4025190cb0b5d48b17907784bfadecf8
ubombar <ufuk@bombar.dev>
2022-12-01 13:26:23 +0100

    add controller

---
+++ gen.sh
@@ -0,0 +1,14 @@
+# ROOT_PACKAGE :: the package (relative to $GOPATH/src) that is the target for code generation
+ROOT_PACKAGE="github.com/ubombar/live-pod-migration"
+# CUSTOM_RESOURCE_NAME :: the name of the custom resource that we're generating client code for
+CUSTOM_RESOURCE_NAME="livepodmigration"
+# CUSTOM_RESOURCE_VERSION :: the version of the resource
+CUSTOM_RESOURCE_VERSION="v1alpha1"
+
+# retrieve the code-generator scripts and bins
+go get -u k8s.io/code-generator/...
+# cd $GOPATH/src/k8s.io/code-generator
+cd $GOPATH/src/k8s.io/code-generator
+
+# run the code-generator entrypoint script
+./generate-groups.sh all "$ROOT_PACKAGE/pkg/client" "$ROOT_PACKAGE/pkg/apis" "$CUSTOM_RESOURCE_NAME:$CUSTOM_RESOURCE_VERSION"
