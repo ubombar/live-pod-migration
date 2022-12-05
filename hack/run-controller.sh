f6c3db53161fb0d382ff93757ed3632adf3433c5
ubombar <ufuk@bombar.dev>
2022-12-05 21:02:35 +0100

    Add optional vendoring

--- hack/run-controller.sh
+++ hack/run-controller.sh
@@ -7,8 +7,14 @@ ROOT_DIR=$( pwd )
 ## Assuming this is the kubeconfig file of the minikube. Change this if you are using something different.
 MINIKUBE_KUBECONFIG="$HOME/.kube/config"
 
-echo "Vendoring..."
-go mod vendor
+vendoring="false"
+
+if [[ $vendoring == "true" ]]
+then 
+    echo "Vendoring..."
+    # go mod vendor
+fi 
+
 
 echo "Building..."
 go build -o ${ROOT_DIR}/bin/controller ${ROOT_DIR}/cmd/livepodmigration
