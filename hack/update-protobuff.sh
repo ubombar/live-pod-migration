51cfc3f41913c32603821858240ef5f75c7fdace
ubombar <ufukbombar@gmail.com>
2022-12-13 16:33:06 +0100

    Tetx

--- hack/update-protobuff.sh
+++ hack/update-protobuff.sh
@@ -4,10 +4,11 @@ echo "Generating protobuffers"
 
 PROTOC="/usr/bin/protoc"
 SRC_DIR=$( pwd )
-PROTO_FILE="$SRC_DIR/pkg/migrator/v1alpha1/migrator_interface.proto"
-DST_DIR="$SRC_DIR/pkg/generated/migrator/v1alpha1/pb"
+
+PROTO_FILE="$SRC_DIR/pkg/proto/adapter.proto"
+DST_DIR="$SRC_DIR/pkg/generatedpb"
 
 # Create directory
-# mkdir -p $DST_DIR
+mkdir -p $DST_DIR
 
 $PROTOC -I=$SRC_DIR --go_out=$DST_DIR $PROTO_FILE
