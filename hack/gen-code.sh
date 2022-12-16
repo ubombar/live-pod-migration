cceab08bb40b8442ab18e840465880969d5a3530
ubombar <ufukbombar@gmail.com>
2022-12-16 20:12:05 +0100

    Finalize gen-code

--- hack/gen-code.sh
+++ hack/gen-code.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/bash
 
-echo "Generating protobuffers"
+echo -n "Generating gRPC stub..."
 
 PROTOC="/usr/bin/protoc"
 SRC_DIR=$( pwd )
 
-PROTO_FILE="$SRC_DIR/pkg/proto/adapter.proto"
-DST_DIR="$SRC_DIR/pkg/generatedpb"
+PROTO_PATH="./pkg"
+PROTO_FILE="$PROTO_PATH/migrator.proto"
+DST_DIR="$SRC_DIR/pkg/grpc"
 
 # Create directory
 mkdir -p $DST_DIR
 
-$PROTOC -I=$SRC_DIR --go_out=$DST_DIR $PROTO_FILE
+$PROTOC --proto_path=$PROTO_PATH --go_out=$DST_DIR --go-grpc_out=$DST_DIR $PROTO_FILE
+
+echo "done!"
