e2e449494256568b0c314b47d9459dd1317d7ca2
ubombar <ufukbombar@gmail.com>
2022-12-19 16:55:08 +0100

    Clear migctl main

--- cmd/migctl/main.go
+++ cmd/migctl/main.go
@@ -3,15 +3,5 @@ package main
 import "github.com/ubombar/live-pod-migration/pkg/migctl/cmd"
 
 func main() {
-
-	// var addrClient, addrServer string
-	// var portClient, portServer int
-
-	// // Default values are for debugging
-	// flag.StringVar(&addrClient, "addrc", "localhost", "Client address")
-	// flag.StringVar(&addrServer, "addrs", "localhost", "Server address")
-	// flag.IntVar(&portClient, "portc", 4545, "Client address")
-	// flag.IntVar(&portServer, "ports", 4546, "Server port")
-
 	cmd.Execute()
 }
