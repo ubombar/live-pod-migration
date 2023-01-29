c0f85b49a23dc627aeecc88566b0ab1b555b0198
ubombar <ufukbombar@gmail.com>
2023-01-29 16:47:21 +0100

    Add migratord installer

--- hack/install-migratord.sh
+++ hack/install-migratord.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/bash
 
-NODES=( "mentor" "mentor-m02" )
-PROFILE="mentor"
+HOSTS=( "bishop.local" "rook.local" )
 
 ./hack/gen-code.sh
 ./hack/build.sh
 
-for node in ${NODES[@]}
+# sudo install migctl /usr/local/bin
+
+for node in ${HOSTS[@]}
 do 
-    echo -n "> Installing migratord on $node..."
+    echo -n "  > Copying migratord on $node..."
 
-    node_ip=$( minikube ip -p $PROFILE -n $node )
-    node_port="4545"
+    scp ./bin/migratord ubombar@$node:~/migratord >/dev/null
 
-    minikube ssh -p $PROFILE -n $node -- "killall migratord" >/dev/null 2>/dev/null
+    # ssh bombar@$node -- "sudo install ~/migratord /usr/local/bin"
 
-    minikube ssh -p $PROFILE -n $node -- "rm -f /home/docker/migratord" >/dev/null 2>/dev/null
+#     node_ip=$( minikube ip -p $PROFILE -n $node )
+#     node_port="4545"
 
-    scp -i "$( minikube ssh-key -p $PROFILE -n $node )" ./bin/migratord docker@$node_ip:/home/docker/migratord >/dev/null
+#     minikube ssh -p $PROFILE -n $node -- "killall migratord" >/dev/null 2>/dev/null
 
-    minikube ssh -p $PROFILE -n $node -- "/home/docker/migratord --address $node_ip --port $node_port 1>logs.txt 2>logs.txt" >/dev/null 2>/dev/null &
+#     minikube ssh -p $PROFILE -n $node -- "rm -f /home/docker/migratord" >/dev/null 2>/dev/null
+
+#     scp -i "$( minikube ssh-key -p $PROFILE -n $node )" ./bin/migratord docker@$node_ip:/home/docker/migratord >/dev/null
+
+#     minikube ssh -p $PROFILE -n $node -- "/home/docker/migratord --address $node_ip --port $node_port 1>logs.txt 2>logs.txt" >/dev/null 2>/dev/null &
 
     echo "done!"
 done
